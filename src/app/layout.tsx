a581b2072ebacc57d0a1f4021ce962dac2fabd3f
Piotr Regulski <bulu901@gmail.com>
2024-04-13 15:13:32 +0200

    added overflow-x-hidden

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${rowdies.className} antialiased bg-gradient-to-r from-lime-200 via-white to-lime-200 `}
+        className={`${rowdies.className} antialiased bg-gradient-to-r from-lime-200 via-white to-lime-200 overflow-x-hidden `}
       >
         <main className="w-full mx-auto lg:my-[4%]  md:p-1">
           
