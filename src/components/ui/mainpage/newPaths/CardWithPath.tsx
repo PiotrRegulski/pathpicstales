946ae6f3fbb6916d84aa0a174ebf04dff7f7169d
Piotr Regulski <bulu901@gmail.com>
2024-04-22 20:47:04 +0200

    adding src props to the component

--- src/components/ui/mainpage/newPaths/CardWithPath.tsx
+++ src/components/ui/mainpage/newPaths/CardWithPath.tsx
@@ -2,23 +2,25 @@ import React from "react";
 import PathCard from "./PathCard";
 
 const CardWithPath = () => {
+
+  const newPathsImages=[]
   return (
     <section className="flex flex-col">
-      <h3 className="text-black bg-white text-center  font-semibold text-xl py-4 px-4 tracking-wide rounded-xl">
+      <h3 className="text-black bg-white text-center  font-semibold text-xl py-4 px-4 tracking-wide rounded-xl mb-12 mt-10">
         Nowe Trasy
       </h3>
-      <div className="  grid grid-cols-4 h-1/2">
-        <div className=" h-full">
-          <PathCard />
+      <div className="  grid grid-cols-1 lg:grid-cols-4 h-1/4">
+        <div>
+          <PathCard src={'/NewPleaceImg/zamekwarszawasm.png'} />
         </div>
         <div>
-          <PathCard />
+          <PathCard  src={'/NewPleaceImg/niegocingiżycko.png'}/>
         </div>
         <div>
-          <PathCard />
+          <PathCard src={'/NewPleaceImg/zamekwarszawasm.png'}/>
         </div>
         <div>
-          <PathCard />
+          <PathCard src={'/NewPleaceImg/zamekwarszawasm.png'}/>
         </div>
       </div>
     </section>
--- src/components/ui/mainpage/newPaths/PathCard.tsx
+++ src/components/ui/mainpage/newPaths/PathCard.tsx
@@ -1,32 +1,40 @@
 import React from "react";
 import Image from "next/image";
+import RatingStar from "@/components/icons/RatingStar";
 
-
-const PathCard = () => {
-    const images = [
-        { id: 1, path: "niegocingiżycko.png" },
-        { id: 2, path: "wisławarszawa.png" },
-        { id: 3, path: "zamekwarszawasm.png" },
-        { id: 4, path: "zamekwarszawasm.png" },
-        { id: 5, path: "zamekwarszawasm.png" },
-        { id: 6, path: "zamekwarszawasm.png" },
-        { id: 7, path: "zamekwarszawasm.png" },
-        // Dodaj więcej obiektów z informacjami o obrazach
-      ];
+import { IoMdBicycle } from "react-icons/io";
+type PathCardProps = {
+  src: string;
+};
+const PathCard: React.FC<PathCardProps> = (props) => {
   return (
-    <div className="flex flex-col items-center justify-between h-96">
+    <div className="flex flex-col items-center justify-between h-96 mb-2 shadow-xl shadow-lime-700/50">
       <div className="relative w-full ">
         <div className="absolute -z-10 w-full h-96 px-2 ">
           <Image
-            src={`/NewPleaceImg/zamekwarszawasm.png`}
+            src={props.src}
             alt="bacground image"
             className="w-full h-full rounded-xl"
             width={1000}
             height={1000}
           />
         </div>
-        <div className="px-3"><p>Coś tam</p></div>
+        <div className="grid grid-cols-1 gap-4 content-between h-96 px-3 ">
+          <div className="py-2 flex ">
+            <IoMdBicycle className="w-12 h-12 border-2 rounded-full px-2 py-2" />
+          </div>
+          <div className="  text-white max-w-max span-2 mb-3 ml-2">
+            <p className="bg-black bg-opacity-75  text-white mb-2 text-lg">
+              Super wycieczka<br></br>Piękne krajobrazy i super miejsca na
+              piknik
+            </p>
 
+            <div className="flex  bg-white rounded-xl text-black max-w-max ">
+              <RatingStar className="ml-2 w-8 h-8" />
+              <p className="pt-1 px-3">5.5/6</p>{" "}
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
